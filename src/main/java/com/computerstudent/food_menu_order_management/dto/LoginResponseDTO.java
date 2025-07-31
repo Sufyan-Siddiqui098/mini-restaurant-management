8efd97d87723f90e6774ac59fbd16bdd4a67e91e
Sufyan Siddiqui <sufyansiddiqui098@gmail.com>
2025-08-01 01:17:44 +0500

    login response dto and ObjectId serializer

---
+++ src/main/java/com/computerstudent/food_menu_order_management/dto/LoginResponseDTO.java
@@ -0,0 +1,32 @@
+package com.computerstudent.food_menu_order_management.dto;
+
+import com.computerstudent.food_menu_order_management.config.ObjectIdSerializer;
+import com.computerstudent.food_menu_order_management.entity.Address;
+import com.computerstudent.food_menu_order_management.enums.UserRole;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class LoginResponseDTO {
+    @JsonSerialize(using = ObjectIdSerializer.class)
+    private ObjectId id;
+    private String email;
+    private String userName;
+    private String firstName;
+    private String lastName;
+    private List<UserRole> roles;
+    private String phone;
+
+    private Address address;
+    private String token;
+
+}
