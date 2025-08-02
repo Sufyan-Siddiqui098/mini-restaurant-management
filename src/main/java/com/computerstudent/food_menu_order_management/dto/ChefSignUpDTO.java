4df314f5e6f606d5314cc40ba977e8137c5f994a
Sufyan Siddiqui <sufyansiddiqui098@gmail.com>
2025-08-03 01:11:18 +0500

    Chef signup DTO and UserResponse DTO

---
+++ src/main/java/com/computerstudent/food_menu_order_management/dto/ChefSignUpDTO.java
@@ -0,0 +1,41 @@
+package com.computerstudent.food_menu_order_management.dto;
+
+import com.computerstudent.food_menu_order_management.enums.UserRole;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChefSignUpDTO {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid Email format")
+    private String email;
+    @NotBlank(message = "Username is required")
+    private String userName;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    @NotBlank(message = "Phone is required")
+    private String phone;
+
+    @NotBlank(message = "First name is required")
+    private String firstName;
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    private List<UserRole> roles ;
+
+    @NotEmpty(message = "At least one specialization is required")
+    private List<String> specialization;
+}
--- src/main/java/com/computerstudent/food_menu_order_management/dto/UserResponseDTO.java
+++ src/main/java/com/computerstudent/food_menu_order_management/dto/UserResponseDTO.java
@@ -1,11 +1,13 @@
 package com.computerstudent.food_menu_order_management.dto;
 
+import com.computerstudent.food_menu_order_management.config.ObjectIdSerializer;
 import com.computerstudent.food_menu_order_management.entity.Address;
 import com.computerstudent.food_menu_order_management.entity.ChefDetails;
 import com.computerstudent.food_menu_order_management.entity.CustomerDetails;
 import com.computerstudent.food_menu_order_management.entity.StaffDetails;
 import com.computerstudent.food_menu_order_management.enums.UserRole;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,6 +20,7 @@ import java.util.List;
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserResponseDTO {
+    @JsonSerialize(using = ObjectIdSerializer.class)
     private ObjectId id;
     private String email;
     private String userName;
@@ -26,6 +29,7 @@ public class UserResponseDTO {
     private List<UserRole> roles;
     private String phone;
 
+    // set specific field - not all field. (reminder)
     private ChefDetails chefDetails;
     private CustomerDetails customerDetails;
     private StaffDetails staffDetails;
