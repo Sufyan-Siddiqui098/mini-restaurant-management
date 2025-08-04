0ed8837d5630b8d9fa5462573c551b1d7b8b96a0
Sufyan Siddiqui <sufyansiddiqui098@gmail.com>
2025-08-05 00:48:31 +0500

    handle case-insensitive role

--- src/main/java/com/computerstudent/food_menu_order_management/enums/UserRole.java
+++ src/main/java/com/computerstudent/food_menu_order_management/enums/UserRole.java
@@ -1,8 +1,20 @@
 package com.computerstudent.food_menu_order_management.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+import java.util.Arrays;
+
 public enum UserRole {
     CUSTOMER,
     ADMIN,
     CHEF,
-    DELIVERY_STAFF
+    DELIVERY_STAFF;
+
+    @JsonCreator
+    public static UserRole fromString(String key){
+        return Arrays.stream(UserRole.values())
+                .filter(role -> role.name().equalsIgnoreCase(key))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Invalid role: " + key));
+    }
 }
