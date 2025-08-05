81070f098c6e87bcad6716d8e673ed7d47a062d8
Sufyan Siddiqui <sufyansiddiqui098@gmail.com>
2025-08-06 01:58:09 +0500

    chef response Dto

---
+++ src/main/java/com/computerstudent/food_menu_order_management/dto/ChefResponseDTO.java
@@ -0,0 +1,30 @@
+package com.computerstudent.food_menu_order_management.dto;
+
+import com.computerstudent.food_menu_order_management.config.ObjectIdSerializer;
+import com.computerstudent.food_menu_order_management.enums.UserRole;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ChefResponseDTO {
+    @JsonSerialize(using = ObjectIdSerializer.class)
+    private ObjectId id;
+    private String email;
+    private String userName;
+    private String firstName;
+    private String lastName;
+    private List<UserRole> roles;
+    private String phone;
+
+    private List<String> specialization;
+
+}
